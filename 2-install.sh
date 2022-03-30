ce39d71e4e35e1cfd3f647c24341407be837e49b
Dustin Loring <dustinwloring1988@gmail.com>
2022-03-30 10:32:33 -0400

    Update 2-install.sh

--- 2-install.sh
+++ 2-install.sh
@@ -4,4 +4,3 @@ cd /tmp
 wget https://kasm-static-content.s3.amazonaws.com/kasm_release_1.10.0.238225.tar.gz
 tar -xf kasm_release*.tar.gz
 sudo nano kasm_release/install.sh
-echo "Run the following command to start the installation process: sudo bash kasm_release/install.sh"
